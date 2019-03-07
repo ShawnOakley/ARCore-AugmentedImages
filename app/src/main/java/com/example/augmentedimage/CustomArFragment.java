b11877a2d06a2d6b323cd775afd2ea10ab5d191f
Shawn Oakley <shawnoakley@gmail.com>
2019-03-06 23:57:34 -0500

    Creates ar fragment

---
+++ app/src/main/java/com/example/augmentedimage/CustomArFragment.java
@@ -0,0 +1,21 @@
+package com.example.augmentedimage;
+
+import com.google.ar.core.Config;
+import com.google.ar.core.Session;
+import com.google.ar.sceneform.ux.ArFragment;
+
+public class CustomArFragment extends ArFragment {
+
+    @Override
+    protected Config getSessionConfiguration(Session session){
+
+       getPlaneDiscoveryController().setInstructionView(null);
+
+       Config config = new Config(session);
+       config.setUpdateMode(Config.UpdateMode.LATEST_CAMERA_IMAGE);
+       session.configure(config);
+       this.getArSceneView().setupSession(session);
+
+       return config;
+    }
+}
--- app/src/main/java/com/example/augmentedimage/MainActivity.java
+++ app/src/main/java/com/example/augmentedimage/MainActivity.java
@@ -5,9 +5,13 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private CustomArFragment arFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        arFragment = (CustomArFragment) getSupportFragmentManager().findFragmentById(R.id.sceneform_fragment);
+        arFragment.getPlaneDiscoveryController().hide();
     }
 }
